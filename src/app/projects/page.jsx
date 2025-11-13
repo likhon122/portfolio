19c1d2c424c33a722e2bb40702d10f78b5bad8d4
likhon122 <md.likhonislam2x@gmail.com>
2025-11-13 13:41:00 +0600

    Add new CV and add one new full stack project info

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -2,12 +2,37 @@ import ClientProjects from "@/components/ClientProjects";
 import slide1 from "@/public/work/thumb1.png";
 import slide2 from "@/public/work/thumb2.png";
 import slide3 from "@/public/work/thumb3.png";
+import slide4 from "@/public/work/thumb5.png";
 
 // Project Data
 const projects = [
   {
     _id: "1",
     num: 1,
+    title: "Template's For Creative Professionals",
+    category: "Full Stack",
+    description:
+      "A portfolio website built with Next.js, Express, Typescript, Tailwind CSS, and Framer Motion.",
+    stack: [
+      { name: "Next.js" },
+      { name: "Express" },
+      { name: "Typescript" },
+      { name: "Tailwind CSS" },
+      { name: "Stripe" },
+      { name: "Shadcn UI" },
+      { name: "JWT" },
+      { name: "MongoDB" },
+      { name: "Mongoose" },
+      { name: "RTK Query" },
+      { name: "Redux Toolkit" },
+    ],
+    image: slide4,
+    live: "https://lz-fly-frontend.vercel.app/",
+    github: "https://github.com/likhon122/lz-fly-frontend",
+  },
+  {
+    _id: "2",
+    num: 2,
     title: "Chat Application",
     category: "Full Stack",
     description:
@@ -25,15 +50,15 @@ const projects = [
       { name: "RTK Query" },
       { name: "Redux Toolkit" },
       { name: "Styled Components" },
-      { name: "Web Push Notifications" }
+      { name: "Web Push Notifications" },
     ],
     image: slide1,
     live: "https://friends-adda.netlify.app",
-    github: "https://github.com/likhon122/Chat-app"
+    github: "https://github.com/likhon122/Chat-app",
   },
   {
-    _id: "2",
-    num: 2,
+    _id: "3",
+    num: 3,
     title: "E-commerce",
     category: "Full Stack",
     description:
@@ -48,15 +73,15 @@ const projects = [
       { name: "Bcrypt" },
       { name: "RTK Query" },
       { name: "Redux Toolkit" },
-      { name: "Styled Components" }
+      { name: "Styled Components" },
     ],
     image: slide2,
     live: "https://carfision.vercel.app/",
-    github: "https://github.com/likhon122/car-fision-frontend"
+    github: "https://github.com/likhon122/car-fision-frontend",
   },
   {
-    _id: "3",
-    num: 3,
+    _id: "4",
+    num: 4,
     title: "Portfolio",
     category: "Frontend",
     description: "My portfolio website built with Next.js and Framer Motion.",
@@ -64,12 +89,12 @@ const projects = [
       { name: "Next.js" },
       { name: "Framer Motion" },
       { name: "Tailwind CSS" },
-      { name: "React Icons" }
+      { name: "React Icons" },
     ],
     image: slide3,
     live: "https://likhonislam.me",
-    github: "https://github.com/likhon122/protfolio"
-  }
+    github: "https://github.com/likhon122/protfolio",
+  },
 ];
 
 // SEO Optimization: generateMetadata function
@@ -92,22 +117,22 @@ export async function generateMetadata() {
           url: "/work/thumb1.png",
           width: 1200,
           height: 630,
-          alt: "Chat Application Thumbnail"
+          alt: "Chat Application Thumbnail",
         },
         {
           url: "/work/thumb2.png",
           width: 1200,
           height: 630,
-          alt: "E-commerce Application Thumbnail"
+          alt: "E-commerce Application Thumbnail",
         },
         {
           url: "/work/thumb3.png",
           width: 1200,
           height: 630,
-          alt: "Portfolio Application Thumbnail"
-        }
+          alt: "Portfolio Application Thumbnail",
+        },
       ],
-      type: "website"
+      type: "website",
     },
     twitter: {
       card: "summary_large_image",
@@ -115,13 +140,13 @@ export async function generateMetadata() {
       description,
       images: [
         {
-          url: "/work/thumb1.png"
-        }
-      ]
+          url: "/work/thumb1.png",
+        },
+      ],
     },
     alternates: {
-      canonical: url
-    }
+      canonical: url,
+    },
   };
 }
 
